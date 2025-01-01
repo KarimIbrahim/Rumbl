fd044d2ac051e1af55d2a1b3d681baee275527c0
Karim Ibrahim <karimibr@amazon.com>
2025-01-01 12:07:46 -0800

    Working user management

--- lib/rumbl_web/controllers/user_html.ex
+++ lib/rumbl_web/controllers/user_html.ex
@@ -3,11 +3,19 @@ defmodule RumblWeb.UserHTML do
 
   alias Rumbl.Accounts
 
+  embed_templates "user/*"
+
+  attr :user, :map, required: true
+
   def first_name(%Accounts.User{name: name}) do
     name
     |> String.split(" ")
     |> Enum.at(0)
   end
 
-  embed_templates "user/*"
+  def user(assigns) do
+    ~H"""
+    <b>{first_name(@user)}</b> ({@user.id})
+    """
+  end
 end
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RumblWeb.Router do
   use RumblWeb, :router
 
+  # alias RumblWeb.UserController
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RumblWeb.Router do
     plug :put_root_layout, html: {RumblWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug RumblWeb.Auth
   end
 
   pipeline :api do
@@ -17,9 +20,9 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/user/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
